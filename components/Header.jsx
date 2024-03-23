f464ebcccda38df3f371074938bda55d891c92d3
mohs-in <in.mohsin@outlook.com>
2024-03-23 23:54:00 +0530

    added semantics to the componenet

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,22 +3,17 @@ import locLogo from '/loc.png'
 import hamburger from '/burger.png'
 function Header() {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li class="logo">
-                        WeBarelyWork
-                    </li>
-                    <li>
-                        <img src={locLogo} class="icon" alt="location" />
-                    </li>
-                    <li id='burger-icon'><img src={hamburger} className="icon"  alt="hamburger icon" /></li>
-                </ul>
-            </nav> 
-            <section>
-                <h1>The best coworking spaces for lazy devs.</h1>
-            </section>
-        </div>    
+        <nav>
+            <ul>
+                <li class="logo">
+                    WeBarelyWork
+                </li>
+                <li>
+                    <img src={locLogo} class="icon" alt="location" />
+                </li>
+                <li id='burger-icon'><img src={hamburger} className="icon"  alt="hamburger icon" /></li>
+            </ul>
+        </nav>  
     )
 }
 
