4481403e640a4ebd67b0e0f66b1aaf2d2673f89e
Mohsin <in.mohsin@outlook.com>
2025-12-14 17:09:08 +0530

    fix: gh-pages deploy

--- components/Title.jsx
+++
@@ -1,7 +0,0 @@
-export default function Title() {
-    return(
-        <section className="title">
-            <h1>The best coworking spaces for lazy devs.</h1>
-        </section>
-    )
-}
--- components/imgs.js
+++
@@ -1,7 +0,0 @@
-export function getImageUrl(img) {
-    return (
-      '/' +
-      img +
-      '.png'
-    );
-  }
